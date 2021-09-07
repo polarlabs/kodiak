ac6783b6d4e1b9d842e8a0e0209e6d940b726f29
tokcum <tobias.mucke@gmail.com>
2021-09-07 18:27:39 +0200

    Improve error handling of file io. Fix all compiler warnings.

--- src/io/file.rs
+++ src/io/file.rs
@@ -1,49 +1,38 @@
+use crate::unit::CRUD;
+
 use serde_json::{json, Map, Value};
 
-use std::fs;
-use std::fs::File;
-use std::io::{ErrorKind, Read};
-use crate::unit::CRUD;
 use std::collections::HashMap;
-use std::ops::Deref;
+use std::fs;
+use std::io::{Read, Write};
 
-// Creates a file
-fn create(file_name: &str) -> File {
-    // todo: implement error handling for file creation
-    // todo: initialize file with {}
-    File::create(file_name).unwrap()
-}
-
-// Reads a file into a Serde Map
 pub fn read(file_name: &str) -> HashMap<String, Box<dyn CRUD>> {
-    // Open file and handle involved errors
-    let file = File::open(file_name.to_string());
-    let mut file = match file {
+    // Open existing or create new file and handle involved errors
+    let mut file = match fs::OpenOptions::new().create(true).read(true).write(true).open(file_name) {
         Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => create(file_name),
-            other_error => panic!("Error opening the file: {:?}", other_error),
-        },
+        Err(e) => panic!("Error opening / creating file {}: {:?}", file_name, e),
     };
 
     // Read file into String
     let mut data = String::new();
-    // todo: implement error handling for empty file
-    file.read_to_string(&mut data);
+    match file.read_to_string(&mut data) {
+        Ok(_) => { println!("Reading state from {} succeeded.", file_name)}
+        Err(e) => { panic!("Fatal {}", e) }
+    }
 
     // Create Map from String
     //let event: Box<dyn WebEvent> = serde_json::from_str(json)?;
     let json: Result<Value, serde_json::Error> = serde_json::from_str(&data);
     let state = match json {
         Ok(json) => {
-            let mut state2 = HashMap::new();
-            let state = json.as_object().unwrap().clone();
-            for object in state.iter() {
-                state2.insert(object.0.clone(), serde_json::from_value(object.1.clone()).unwrap());
+            let mut state = HashMap::new();
+            let map = json.as_object().unwrap().clone();
+            for record in map.iter() {
+                state.insert(record.0.clone(), serde_json::from_value(record.1.clone()).unwrap());
             }
-            state2
+            state
         },
-        Err(error) => {
+        Err(_error) => {
             // todo: improve error handling, e.g. log parsing error, which might be a fatal error
             HashMap::new()
         },
@@ -53,31 +42,23 @@ pub fn read(file_name: &str) -> HashMap<String, Box<dyn CRUD>> {
 }
 
 pub fn write(file_name: &str, state: &HashMap<String, Box<dyn CRUD>>) {
-    let mut state2 = Map::new();
+    // Open existing or create new file and handle involved errors
+    let mut file = match fs::OpenOptions::new().create(true).read(true).write(true).open(file_name) {
+        Ok(file) => file,
+        Err(e) => panic!("Error opening / creating file {}: {:?}", file_name, e),
+    };
 
-    for object in state.iter() {
-        state2.insert(object.0.clone(), json!(object.1));
+    let mut map = Map::new();
+    for record in state.iter() {
+        map.insert(record.0.clone(), json!(record.1));
     }
 
-    let data = json!(state2).to_string();
-    fs::write(file_name, data.as_bytes()).unwrap()
+    let data = json!(map).to_string();
+    match file.write_all(data.as_bytes()) {
+        Ok(()) => println!("State persisted to disk in file {}", file_name),
+        Err(e) => panic!("Error writing to file {}: {:?}", file_name, e),
+    }
 
     // todo: beautify output to file
     // todo: add final line break to file
-
-    // Open file and handle involved errors
-    /*
-    let file = File::open(file_name.to_string());
-    let mut file = match file {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => create(file_name),
-            other_error=> panic!("Error opening the file: {:?}", other_error),
-        },
-    };
-    */
-    /*
-    let mut file = File::open(file_name.to_string()).unwrap();*/
-
-    //file.write_all(data.as_bytes()).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,15 @@ mod unit;
 use unit::factory;
 use unit::UnitType;
 use unit::CRUD;
-use unit::{Asset, Task, User};
 
 use io::file::read as file_read;
 use io::file::write as file_write;
 
 #[macro_use]
 extern crate clap;
-use clap::{App, Arg, ArgMatches};
+use clap::App;
 
-#[macro_use]
-extern crate serde_json;
-use serde_json::{Map, Value};
 use std::collections::HashMap;
-use std::ops::Deref;
 
 fn create<'a>(state: &'a mut HashMap<String, Box<dyn CRUD>>, unit_type: UnitType, name: &str) -> &'a Box<dyn CRUD> {
     let unit = factory(unit_type, name);
@@ -96,7 +91,7 @@ fn main() {
         ("delete", Some(sub_m)) => {
             let key = sub_m.value_of("KEY").unwrap().to_owned();
             match delete(&mut state, key.as_str()) {
-                Some(v) => println!("Unit {} deleted.", key),
+                Some(_unit) => println!("Unit {} deleted.", key),
                 None => println!("Unit {} not found.", key),
             }
         }
@@ -104,25 +99,6 @@ fn main() {
             unreachable!()
         }
     }
-
-    /*
-    let a = Asset::new(Some("HOST001"));
-    println!("{:?}", a);
-
-    let t = Task::new(Some("Install Server HOST002"), Some("Open"));
-    println!("{:?}", t);
-
-    let u = User::new(Some("tobias.mucke@gmail.com"));
-    println!("{:?}", u);
-
-    state.insert(a.key(), json!(a));
-    state.insert(t.key(), json!(t));
-    state.insert(u.key(), json!(u));
-    */
-    /*
-    let mut state: HashMap<&str, Box<dyn CRUD>> = HashMap::new();
-    state.insert(t1.key, t1);
-     */
 
     file_write("./kodiak.file", &state);
 }
